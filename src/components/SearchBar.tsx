71b00381c4d26e41b52394d0599f83266cf8e720
SweetBabyDave <SweetBabyDave@gmail.com>
2023-01-27 19:16:47 -0700

    Finished up the CSS. This is done.

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -8,7 +8,7 @@ export function SearchBar({name, setName}: SearchBarProps) {
     return (
         <div id="searchbar">
             <label>
-                <input type="text" value={name} onChange={e => setName(e.target.value)} placeholder="Search Quotes"/>
+                <input id="searchbar" type="text" value={name} onChange={e => setName(e.target.value)} placeholder="Search Quotes"/>
             </label>
         </div>
     );
